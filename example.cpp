04f435df6d714ccf0948ca13079150db2e7b6a98
tarunmahi <tarunmahendra007@gmail.com>
2025-07-29 11:41:04 +0530

    first_push

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int add(int x , int y){
+        return x+y;
+    }
+};
+
+int main(){
+    Solution s;
+    int val =s.add(3,5);
+    cout<<val;
+}
